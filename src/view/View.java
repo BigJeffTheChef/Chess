fd4d150e26ef35492b486117688c31d6ce1e6fbd
BigJeffTheChef <marleygpeter@gmail.com>
2022-02-01 00:28:19 +0000

    begin creating a temporary introduction Stage

--- src/view/View.java
+++ src/view/View.java
@@ -7,7 +7,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 
 
 public class View extends Application {
@@ -18,29 +20,55 @@ public class View extends Application {
 
 	public static void main(String[] args) {
 		Controller.initController();
-		System.out.println("end of main");
+		System.out.println("Goodbye! End of main()");
 	}
 	
 	///////////////////////////////////////////////////
 	// Application launch()							//
 	/////////////////////////////////////////////////
 	
+	/**
+	 * A static method that allows the Controller to control execution.
+	 * Controller ensures the Model has been created successfully, or forces
+	 * the program to terminate by allowing the program to reach end of main()
+	 */
 	public static void launchGUI() {
 		launch();
 	}
 
+	/**
+	 * Initialise the GUI
+	 */
 	@Override
 	public void start(Stage stage) throws Exception {
 		System.out.println("start() called");
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Introduction.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("windows/Introduction.fxml"));
 			Scene scene = new Scene(root, 600, 600);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("windows/Introduction.css").toExternalForm());
 			stage.setResizable(false);
+			stage.setOnCloseRequest(event -> {
+				quit(stage);
+				event.consume();
+			});
 			stage.setScene(scene);
 			stage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Quit the game with a confirmation alert
+	 * @param stage The stage to quit from
+	 */
+	public void quit(Stage stage) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Quit");
+		alert.setHeaderText("You are about to quit!");
+		alert.setContentText("Are you sure?");
+		if (alert.showAndWait().get() == ButtonType.OK) {
+			stage.close();
 		}
 	}
 }
---
+++ src/view/windows/IntroductionController.java
@@ -0,0 +1,30 @@
+package view.windows;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class IntroductionController {
+	
+	@FXML
+	private Button quitButton;
+	@FXML
+	private GridPane introductionPane;
+	Stage stage;
+	
+	public void quit(ActionEvent e) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Quit");
+		alert.setHeaderText("You are about to quit!");
+		alert.setContentText("Are you sure?");
+		if (alert.showAndWait().get() == ButtonType.OK) {
+			stage = (Stage) introductionPane.getScene().getWindow();
+			stage.close();
+		}
+	}
+}
